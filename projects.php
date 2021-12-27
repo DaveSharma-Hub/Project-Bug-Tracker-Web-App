24f94000e1ed6ca86cdf76618cebce81481eb143
Dave Sharma <81478885+DaveSharma-Hub@users.noreply.github.com>
2021-12-27 10:08:03 -0700

    Update projects.php

--- projects.php
+++ projects.php
@@ -149,13 +149,13 @@ table{
 <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
     <button class="w3-bar-item w3-button w3-large"
     onclick="w3_close()">Close &times;</button>
-    <a href="http://localhost:3000/Users/Daves/Documents/Practice/tracker/editInfo.php" class="w3-bar-item w3-button">Edit Information
+    <a href="editInfo.php" class="w3-bar-item w3-button">Edit Information
         </a>
-    <a href="http://localhost:3000/Users/Daves/Documents/Practice/tracker/notifications.php" class="w3-bar-item w3-button">Notifications</a>
-    <a href="http://localhost:3000/Users/Daves/Documents/Practice/tracker/messages.php" class="w3-bar-item w3-button">Direct Message</a>
-    <a href="http://localhost:3000/Users/Daves/Documents/Practice/tracker/projects.php" class="w3-bar-item w3-button">Projects</a>
-    <a href="http://localhost:3000/Users/Daves/Documents/Practice/tracker/bug.php" class="w3-bar-item w3-button">Bugs</a>
-    <a href="http://localhost:3000/Users/Daves/Documents/Practice/tracker/main.php" class="w3-bar-item w3-button">Main Page</a>
+    <a href="notifications.php" class="w3-bar-item w3-button">Notifications</a>
+    <a href="messages.php" class="w3-bar-item w3-button">Direct Message</a>
+    <a href="projects.php" class="w3-bar-item w3-button">Projects</a>
+    <a href="bug.php" class="w3-bar-item w3-button">Bugs</a>
+    <a href="main.php" class="w3-bar-item w3-button">Main Page</a>
     <a href="index.html" class="w3-bar-item w3-button">Logout</a>
 
 </div>
@@ -198,12 +198,7 @@ table{
             // echo "<th>Project #</th><th>Project Description</th><th>Tools</th></tr><tr>";
 
             while($data = ($stmt_result->fetch_array())){
-                    // echo "<h2>Project ". $num.": ";
-                    // echo $data['projectDesc']." ";
-                    // echo " Tools : ";
-                    // echo $data['tools'];
-                    // echo "<h2>";
-                    // echo "<br>";
+                   
                     
                 echo "<tr>";
                 echo "<td>"."Project $num"."</td>";
@@ -214,29 +209,9 @@ table{
                 $num=$num+1;
                 }
             echo "</table>";
-            // if($stmt_result->num_rows > 0){
-            //     $data = $stmt_result->fetch_assoc();
-            //     for($x=0;$x<$stmt_result->num_rows;$x++){
-            //         $num =$x+1;
-            //         echo "<h2>Project ". $num.": ";
-            //         echo $data['projectDesc']." ";
-            //         echo " Tools : ";
-            //         echo $data['tools'];
-            //         echo "<h2>";
-            //         echo "<br>";
-            //     }
-            // }
+           
         }
-    
 
-
-        // echo "<h1>Projects: ";
-        // echo $email;
-        // echo "</h1>";
-
-        // session_start();
-        // $email =$_SESSION['formdata'];
-        // session_abort();
     ?>
     </div>
   </div>
@@ -252,7 +227,7 @@ table{
     $_SESSION['formdata'] = $email; 
     ?>
 
-  <form action="http://localhost:3000/Users/Daves/Documents/Practice/tracker/addProject.php"  method="post" style="border:1px solid #ccc">
+  <form action="addProject.php"  method="post" style="border:1px solid #ccc">
         <div class="container">
           <h1>Add Project</h1>
           <p>Enter Project Information Below</p>
@@ -280,7 +255,7 @@ table{
       </form>
 
 
-      <form action="http://localhost:3000/Users/Daves/Documents/Practice/tracker/editProject.php"  method="post" style="border:1px solid #ccc">
+      <form action="editProject.php"  method="post" style="border:1px solid #ccc">
         <div class="container">
           <h1>Edit Project</h1>
           <p>Enter New Project Information Below</p>
@@ -307,7 +282,7 @@ table{
         </div>
       </form>
 
-      <form action="http://localhost:3000/Users/Daves/Documents/Practice/tracker/deleteProject.php"  method="post" style="border:1px solid #ccc">
+      <form action="deleteProject.php"  method="post" style="border:1px solid #ccc">
         <div class="container">
           <h1>Delete Project</h1>
           <p>Delete project</p>
@@ -323,22 +298,6 @@ table{
       </form>
     </div>
 
-    <!-- <div id="content">
-        <form method="POST" 
-                action="http://localhost:3000/Users/Daves/Documents/Practice/tracker/addPhoto.php" 
-                enctype="multipart/form-data">
-            <input type="file" 
-                    name="uploadfile" 
-                    value="" />
-
-            <div>
-                <button type="submit"
-                        name="upload">
-                    UPLOAD
-                </button>
-            </div>
-        </form>
-    </div> -->
   
   <script>
   function w3_open() {
