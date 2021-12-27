52660ef95e8ba4af84721189956a7f0eb6e76f73
Dave Sharma <81478885+DaveSharma-Hub@users.noreply.github.com>
2021-12-27 09:51:12 -0700

    Update addProject.php

--- addProject.php
+++ addProject.php
@@ -6,13 +6,10 @@
     $tools = $_POST['tools'];
     $startDate =$_POST['sd'];
     $endDate =$_POST['ed'];
-    // echo $email;
-//     if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {  
-//         echo 'We don\'t have mysqli!!!';  
-//  } else {  
-//      echo 'mysqli is installed';
-//  }
-    $conn= new mysqli("localhost","dave(2)","ensf409","tracker");
+
+    $username="";
+    $password="";
+    $conn= new mysqli("localhost",$username,$password,"tracker");
     
 
     if($conn->connect_error){
